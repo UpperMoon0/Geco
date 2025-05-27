a427e45c8fadb9832265d4e3313ad29b4d9d7031
NsTut <vonguyenminhnhatanphu@gmail.com>
2025-05-28 02:34:53 +0700

    refactor(Geko.java): format logger initialization and improve code readability; remove obsolete tree configuration files

--- common/src/main/java/com/nstut/Geko.java
+++ common/src/main/java/com/nstut/Geko.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 
 public class Geko {
     public static final String MOD_ID = "geko";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);    public static void init() {
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    
+    public static void init() {
         LOGGER.info("Geko mod initializing...");
         ModBlocks.register();
         ModCreativeTabs.register();
